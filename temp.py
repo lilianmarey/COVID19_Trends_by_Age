76e0c78d98e3c423c53edf5ca4a6a83b67a89819
lilianmarey <lilian.marey@hotmail.fr>
2020-11-17 19:26:11 +0100

    added usa states, only problem with oklahoma

--- temp.py
+++ temp.py
@@ -19,14 +19,9 @@ df5 = pd.read_csv('data/preprocessed_data_update_5.csv')
 print(1)
 
 df6 = pd.read_csv('data/preprocessed_data_update_6.csv')
-df7 = pd.read_csv('data/preprocessed_data_update_7.csv')
-df8 = pd.read_csv('data/preprocessed_data_update_8.csv')
-df9 = pd.read_csv('data/preprocessed_data_update_9.csv')
 print(1)
 
 
-df10 = pd.read_csv('data/preprocessed_data_update_10.csv')
-df11 = pd.read_csv('data/preprocessed_data_update_11.csv')
 
 print(11111)
 
@@ -51,14 +46,12 @@ print(1)
 
 result = pd.merge(result, df5, how='outer')
 result = pd.merge(result, df6, how='outer')
-result = pd.merge(result, df7, how='outer')
-result = pd.merge(result, df8, how='outer')
-result = pd.merge(result, df9, how='outer')
-result = pd.merge(result, df10, how='outer')
-result = pd.merge(result, df11, how='outer')
 
 print(111111)
 
+result = pd.merge(result, pd.read_csv('data/preprocessed_data.csv'), how='outer')
+
+print('hihi')
 result.to_csv('data/preprocessed_data.csv')
 
 
